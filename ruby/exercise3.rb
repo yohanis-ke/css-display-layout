1e89076602f2d996141597475d60daebaba91436
yohanis-ke <yohanes.ke@gmail.com>
2018-07-16 15:26:57 -0400

    worked on asking the age and calculate when the user born

--- ruby/exercise3.rb
+++ ruby/exercise3.rb
@@ -6,3 +6,7 @@ user_name2=gets.chomp
 puts user_name2 == "fabio"
 puts "Hello #{user_name}"
 puts "Hello #{user_name2} again"
+puts "How old are you?"
+age=gets.chomp
+age=age.to_i
+puts "hh... you born in #{2018-age}"
