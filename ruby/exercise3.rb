09dd68bac256e8343af19f88ffff82042c3b16e5
yohanis-ke <yohanes.ke@gmail.com>
2018-07-16 15:16:40 -0400

    i played with get and get chomp and i understand the diffrence.

--- ruby/exercise3.rb
+++ ruby/exercise3.rb
@@ -1,6 +1,8 @@
 puts "What is your name?"
 user_name= gets
+puts user_name == "fabio"
 puts "What is your second name?"
 user_name2=gets.chomp
+puts user_name2 == "fabio"
 puts "Hello #{user_name}"
-puts "hello #{user_name2} too"
+puts "Hello #{user_name2} again"
