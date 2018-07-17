c9367e6dbbe51070bdb6514e6cdc33216f01ef8c
yohanis-ke <yohanes.ke@gmail.com>
2018-07-16 23:05:59 -0400

    upgread to handle if the user enter invalid value

--- ruby/exercise5.rb
+++ ruby/exercise5.rb
@@ -2,15 +2,17 @@
 walk=true
 ran=true
 distance=0
-while ran||walk
+while ran && walk
 puts "Would you like to walk or run?"
   action=gets.chomp
   if action=="ran"
    distance += 6
   elsif action=="walk"
    distance += 1
-   elsif
+ elsif action=="go home"
      walk=!walk && ran=!ran
+   else
+     puts "you inter invalid value"
    end
    puts "Distance from home is #{distance}km."
 end
